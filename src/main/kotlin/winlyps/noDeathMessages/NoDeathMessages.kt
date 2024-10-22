7c01003dcd64490e1923555c4f5c0abf173dacf3
Winlypse <nts3@o2.pl>
2024-10-22 23:34:47 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noDeathMessages/NoDeathMessages.kt
@@ -0,0 +1,14 @@
+package winlyps.noDeathMessages
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoDeathMessages : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
